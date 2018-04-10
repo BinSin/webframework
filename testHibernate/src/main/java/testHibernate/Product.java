a1153718c945cba5f00b0fef026da09f3882c5e5
BinSin <binyong318@gmail.com>
2018-04-10 22:35:33 +0900

    18/4/10

--- testHibernate/src/main/java/testHibernate/Product.java
+++ testHibernate/src/main/java/testHibernate/Product.java
@@ -3,6 +3,7 @@ package testHibernate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+//***import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,12 +13,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity // 이 클래스에 해당되는 내용을 Hibernate가 인지하고 이를 테이블로 만들어 준다.
-@Table(name="product") // 테이블 이름 지정, 안하면 클레스 이름이 된다.)
+@Table(name="Product") // 테이블 이름 지정, 안하면 클레스 이름이 된다.)
 public class Product {
 	@Id
-	@GeneratedValue // 레코드의 primary key 지정
+	@GeneratedValue  // 레코드의 primary key 지정
 	@Column(name="product_id") // column 이름 지정, 안하면 변수가 이름이 된다.
-	public int id;
+	private int id;
 	
 	private String name;
 	
